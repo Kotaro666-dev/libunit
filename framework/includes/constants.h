c765b4397035d98ee2e683a45b4b9b4b580c13f2
Kotaro Kamashima <kotaro0726@gmail.com>
2021-05-08 12:34:42 +0900

    Update: Add basics files

---
+++ framework/includes/constants.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   constants.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/08 12:26:25 by kkamashi          #+#    #+#             */
+/*   Updated: 2021/05/08 12:29:56 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONSTANTS_H
+# define CONSTANTS_H
+
+/*
+** RESULT MESSAGE
+*/
+
+# define MSG_OK "OK"
+# define MSG_KO "KO"
+# define MSG_SEGV "SEGV"
+# define MSG_BUSE "BUSE"
+
+#endif
---
+++ framework/includes/libunit.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libunit.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/08 12:23:27 by kkamashi          #+#    #+#             */
+/*   Updated: 2021/05/08 12:26:14 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBUNIT_H
+# define LIBUNI_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <signal.h>
+
+#endif

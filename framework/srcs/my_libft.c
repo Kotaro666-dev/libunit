75aa1391bb357dcdc41566367973f6d57ef1029a
Kotaro Kamashima <kotaro0726@gmail.com>
2021-05-10 08:29:08 +0900

    Modify: Rename file
    - printf系の関数をprint_utilitiesに移したため、こちらのファイル名をmy_libftに変更。
    - 移行はlibft系の関数用に使っていく

--- framework/srcs/my_libft.c
+++ framework/srcs/my_libft.c
@@ -6,34 +6,11 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 17:41:44 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/05/09 21:01:34 by kefujiwa         ###   ########.fr       */
+/*   Updated: 2021/05/10 08:22:17 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "libunit.h"
-
-void	print_result_fd(int result, int fd)
-{
-	if (result == ST_OK)
-		my_putendl_fd(MSG_OK, fd);
-	else if (result == ST_KO)
-		my_putendl_fd(MSG_KO, fd);
-	else if (result == ST_SEGV)
-		my_putendl_fd(MSG_SEGV, fd);
-	else if (result == ST_BUSE)
-		my_putendl_fd(MSG_BUSE, fd);
-	else if (result == ST_ABORT)
-		my_putendl_fd(MSG_ABORT, fd);
-	else if (result == ST_FPE)
-		my_putendl_fd(MSG_FPE, fd);
-}
-
-void	print_name_fd(char *name, int fd)
-{
-	my_putstr_fd("	> ", fd);
-	my_putstr_fd(name, fd);
-	my_putstr_fd(" : ", fd);
-}
 
 int		my_strlen(char *str)
 {

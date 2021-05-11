0f8ac00c763650d9d691181289bb76e2d327daed
Kotaro Kamashima <kotaro0726@gmail.com>
2021-05-11 13:54:25 +0900

    Modify: Add ampersand

--- tests/srcs/01_ko_test/00_launcher.c
+++ tests/srcs/01_ko_test/00_launcher.c
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 08:07:05 by kefujiwa          #+#    #+#             */
-/*   Updated: 2021/05/11 13:30:13 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/05/11 13:52:43 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int ko_launcher(void)
 	testlist = NULL;
 	my_putendl_fd("KO_TEST:", STDOUT_FILENO);
 	my_putendl_fd("KO_TEST:", g_fd);
-	load_test(&testlist, "STRLEN test", strlen_test);
-	load_test(&testlist, "STRNCMP test", strncmp_test);
-	load_test(&testlist, "STRCHR test", strchr_test);
-	load_test(&testlist, "STRRCHR test", strrchr_test);
+	load_test(&testlist, "STRLEN test", &strlen_test);
+	load_test(&testlist, "STRNCMP test", &strncmp_test);
+	load_test(&testlist, "STRCHR test", &strchr_test);
+	load_test(&testlist, "STRRCHR test", &strrchr_test);
 	return (launch_tests(&testlist));
 }

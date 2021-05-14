262f93ff2f2ea092ee8fddbb280fca28e17c9b63
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-05-14 13:56:06 +0900

    test(06_abort_test): remove in_time_test from launcher
    #53

--- tests/srcs/06_alarm_test/00_launcher.c
+++ tests/srcs/06_alarm_test/00_launcher.c
@@ -6,7 +6,7 @@
 /*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 08:10:34 by kefujiwa          #+#    #+#             */
-/*   Updated: 2021/05/14 13:49:43 by kefujiwa         ###   ########.fr       */
+/*   Updated: 2021/05/14 13:54:14 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int alarm_launcher(void)
 	testlist = NULL;
 	printf("ALARM:\n");
 	my_putendl_fd("ALARM:", g_fd);
-	load_test(&testlist, "IN TIME test", &in_time_test);
 	load_test(&testlist, "TIMEOUT test", &timeout_test);
 	run_tests(&testlist);
 	display_results(&testlist);

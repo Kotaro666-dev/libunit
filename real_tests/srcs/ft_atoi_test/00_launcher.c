4d001e882ee2feb617811728d22aab83a9a6511d
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-05-14 16:56:34 +0900

    style(real_tests): apply norminette rules to all functions of real_tests
    #48

--- real_tests/srcs/ft_atoi_test/00_launcher.c
+++ real_tests/srcs/ft_atoi_test/00_launcher.c
@@ -6,40 +6,45 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/08 16:42:26 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/05/14 14:41:04 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/05/14 16:45:31 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "real_tests.h"
 
-int ft_atoi_launcher(void)
+static void	load_testlist(t_unit_test **testlist)
+{	
+	load_test(testlist, "ft_atoi(\"0\")", &zero_test);
+	load_test(testlist, "ft_atoi(\"546:5\")", &nan_in_the_middle_test);
+	load_test(testlist, "ft_atoi(\"-9667\")", &negative_number_test);
+	load_test(testlist, "ft_atoi(\"+289\")", &positive_number_test);
+	load_test(testlist, "ft_atoi(\"077\")", &prefix_single_zero_test);
+	load_test(testlist, "ft_atoi(\"0002543\")", &prefix_multi_zero_test);
+	load_test(testlist, "ft_atoi(\"+-112\")", &plus_minus_test);
+	load_test(testlist, "ft_atoi(\"-+258\")", &minus_plus_test);
+	load_test(testlist, "ft_atoi(\"--19\")", &double_minus_test);
+	load_test(testlist, "ft_atoi(\"++779\")", &double_plus_test);
+	load_test(testlist, "ft_atoi(\"+87+11\")", &plus_in_the_middle_test);
+	load_test(testlist, "ft_atoi(\"-12-76\")", &minus_in_the_middle_test);
+	load_test(testlist, "ft_atoi(\"\\t\\n\\r\\v\\f  5342 \\n\")",
+		&is_space_test);
+	load_test(testlist, "ft_atoi(\"2147483647\")", &maximum_test);
+	load_test(testlist, "ft_atoi(\"-2147483648\")", &minimum_test);
+	load_test(testlist, "ft_atoi(\"\\t\\ng112 \\n\")", &nan_after_space_test);
+	load_test(testlist, "ft_atoi(\"\\n\\n  -77\\b9 5g\")", &backspace_test);
+	load_test(testlist, "ft_atoi(\"\")", &empty_string_test);
+	load_test(testlist, "ft_atoi(\"-\")", &solo_minus_test);
+	load_test(testlist, "ft_atoi(\"+\")", &solo_plus_test);
+}
+
+int	ft_atoi_launcher(void)
 {
-	t_unit_test *testlist;
+	t_unit_test	*testlist;
 
 	testlist = NULL;
 	my_putendl_fd("FT_ATOI_TEST:", STDOUT_FILENO);
 	my_putendl_fd("FT_ATOI_TEST:", g_fd);
-	load_test(&testlist, "ft_atoi(\"0\")", &zero_test);
-	load_test(&testlist, "ft_atoi(\"546:5\")", &nan_in_the_middle_test);
-	load_test(&testlist, "ft_atoi(\"-9667\")", &negative_number_test);
-	load_test(&testlist, "ft_atoi(\"+289\")", &positive_number_test);
-	load_test(&testlist, "ft_atoi(\"077\")", &prefix_single_zero_test);
-	load_test(&testlist, "ft_atoi(\"0002543\")", &prefix_multi_zero_test);
-	load_test(&testlist, "ft_atoi(\"+-112\")", &plus_minus_test);
-	load_test(&testlist, "ft_atoi(\"-+258\")", &minus_plus_test);
-	load_test(&testlist, "ft_atoi(\"--19\")", &double_minus_test);
-	load_test(&testlist, "ft_atoi(\"++779\")", &double_plus_test);
-	load_test(&testlist, "ft_atoi(\"+87+11\")", &plus_in_the_middle_test);
-	load_test(&testlist, "ft_atoi(\"-12-76\")", &minus_in_the_middle_test);
-	load_test(&testlist, "ft_atoi(\"\\t\\n\\r\\v\\f  5342 \\n\")",
-			&is_space_test);
-	load_test(&testlist, "ft_atoi(\"2147483647\")", &maximum_test);
-	load_test(&testlist, "ft_atoi(\"-2147483648\")", &minimum_test);
-	load_test(&testlist, "ft_atoi(\"\\t\\ng112 \\n\")", &nan_after_space_test);
-	load_test(&testlist, "ft_atoi(\"\\n\\n  -77\\b9 5g\")", &backspace_test);
-	load_test(&testlist, "ft_atoi(\"\")", &empty_string_test);
-	load_test(&testlist, "ft_atoi(\"-\")", &solo_minus_test);
-	load_test(&testlist, "ft_atoi(\"+\")", &solo_plus_test);
+	load_testlist(&testlist);
 	run_tests(&testlist);
 	display_results(&testlist);
 	append_report(&testlist);
--- real_tests/srcs/ft_strncmp_test/06_long_string.c
+++ real_tests/srcs/ft_strncmp_test/06_long_string.c
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 13:56:19 by kkamashi          #+#    #+#             */
-/*   Updated: 2021/05/14 14:38:58 by kkamashi         ###   ########.fr       */
+/*   Updated: 2021/05/14 16:55:41 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int	long_string(void)
 {
-	char *str1 = "UrZ1Sf1YVg6j2gFgeL5vqeM0sybEP2t9koEUFjihUGvZZie3qRmGdF3TbsN \
-					JN69KaC0jZ35iIXylryOeJIBEdaoj4bKs7SodcOAK";
-	char *str2 = "UrZ1Sf1YVg6j2gFgeL5vqeM0sybEP2t9koEUFjihUGvZZie3qRmGdF3TbsN \
-					JN69KaC0jZ35iIXylryOeJIBEdaoj4bKs7SodcOAK";
+	const char	*str1 = "UrZ1Sf1YVg6j2gFgeL5vqeM0sybEP2t9koEUFjihUGvZZie \
+		3qRmGdF3TbsNJN69KaC0jZ35iIXylryOeJIBEdaoj4bKs7SodcOAK";
+	const char	*str2 = "UrZ1Sf1YVg6j2gFgeL5vqeM0sybEP2t9koEUFjihUGvZZie \
+		3qRmGdF3TbsNJN69KaC0jZ35iIXylryOeJIBEdaoj4bKs7SodcOAK";
 
 	if (ft_strncmp(str1, str2, 100) == strncmp(str1, str2, 100))
 		return (0);
